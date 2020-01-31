e456e492db13402b9701498251157eeaa53fd839
kiranjc <jkiranc@gmail.com>
2020-01-31 11:17:00 +0530

    On branch master

--- src/App.js
+++ src/App.js
@@ -67,8 +67,6 @@ function App() {
             </ul>
           </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
           <Switch>
             <Route path="/cost">
               <Cost />
