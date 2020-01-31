d9ae490a5d7194464b4443980e7acc73b1336c82
kiranjc <jkiranc@gmail.com>
2020-01-31 15:34:54 +0530

    On branch master

--- src/App.js
+++ src/App.js
@@ -36,6 +36,10 @@ import {
   Link
 } from "react-router-dom";
 
+import notificationicon from './images/notification.svg';
+import alerticon from './images/alert.svg';
+import wftlogo from './images/wftlogo.png';
+
 
 
 function App() {
@@ -56,7 +60,8 @@ function App() {
           </div>
         </div>
       </div> */}
-      <Router>
+
+      {/* <Router>
         <div>
           <nav>
             <ul>
@@ -78,11 +83,9 @@ function App() {
             </Route>
           </Switch>
         </div>
-      </Router>
+      </Router> */}
 
-      {/* <Performance /> */}
-
-
+      <Performance />
     </div>
   );
 }
@@ -112,17 +115,130 @@ function Cost() {
 
 function Performance() {
   return (
-    <div className="row" style={{ padding: "1%" }}>
-      <div className="col-lg-4"><Map /></div>
-      <div className="col-lg-8"><TimeLinePiee /></div>
-      <div className="col-lg-6"><Canvas /></div>
-      <div className="col-lg-6"><MemoryUsageCanvas /></div>
-      <div className="col-lg-6"><LiveNetworkActivity /></div>
-      <div className="col-lg-6"><DiskActivityCanvas /></div>
-      {/* <div className="col-lg-6"><MemoryUsage /></div>
+    <div className="container1">
+      <div className="row">
+        <div className="col-lg-2">
+          <Sidebar />
+        </div>
+        <div className="col-lg-10">
+          <Navbar />
+          <div className="row" style={{ padding: "1%" }}>
+            <div className="col-lg-4"><Map /></div>
+            <div className="col-lg-8"><TimeLinePiee /></div>
+            <div className="col-lg-6"><Canvas /></div>
+            <div className="col-lg-6"><MemoryUsageCanvas /></div>
+            <div className="col-lg-6"><LiveNetworkActivity /></div>
+            <div className="col-lg-6"><DiskActivityCanvas /></div>
+            {/* <div className="col-lg-6"><MemoryUsage /></div>
         <div className="col-lg-6"><NetworkActivity /></div>
         <div className="col-lg-6"><DiskUsage /></div> */}
+          </div>
+        </div>
+      </div>
     </div>
 
   );
 }
+
+function Sidebar() {
+  return (
+    <div style={{ backgroundColor: "#244A90", height: "100vh", color: "white", width: "17.1%", position: "fixed" }}>
+      <div className="logo">
+        <img src={wftlogo} style={{ width: "100%" }}></img>
+      </div>
+      <div className="side-options">
+        <div className="subscriptions">
+          <h5>SUBSCRIPTION</h5>
+          <p>WFT-SAP-EA</p>
+        </div>
+        <div className="landscapes">
+          <h5>LANDSCAPES</h5>
+          <ul>
+            <li className="liClass">PROD</li>
+            <li className="liClass">QAS</li>
+            <li className="liClass">STG</li>
+            <li className="liClass">DEV</li>
+            <li className="liClass">SBX</li>
+          </ul>
+        </div>
+        <div className="virtual-machines">
+          <h5>VIRTUAL MACHINES</h5>
+          <div className="lableClass">
+            <label>
+              <input type="checkbox" />
+              <span>VM1</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM2</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM3</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM4</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM5</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM6</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM7</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM8</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM9</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>VM10</span>
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" />
+              <span>Red</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Navbar() {
+  return (
+    <div style={{ backgroundColor: "#3c3c3c", height: "8vh", color: "white" }}>
+      <div className="row">
+        <div className="col-lg-10">
+          <h3 className="wftheading">Wharfedale Cloud Analyzer</h3>
+        </div>
+        <div className="col-lg-2">
+          <div className="actions-notify">
+            <img className="alerticon" src={alerticon}></img>
+            <img className="alerticon" src={notificationicon}></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
